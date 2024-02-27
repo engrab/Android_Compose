890b2cde6aa637d6f126162e5e13dd71b177c644
Engr Bilawal Hussain <35764126+engrab@users.noreply.github.com>
2024-02-27 06:22:08 +0500

    add Bottom Sheet

---
+++ app/src/main/java/com/example/composetutorial/screens/BottomSheetExample.kt
@@ -0,0 +1,87 @@
+package com.example.composetutorial.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun ShowBottomSheet() {
+
+    var showSheet by remember {
+        mutableStateOf(false)
+    }
+    val sheetState = rememberModalBottomSheetState()
+    val scop = rememberCoroutineScope()
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {
+                    showSheet = true
+
+                 },
+
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "add")
+                Text(text = "Open Sheet")
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+
+            if (showSheet){
+                ModalBottomSheet(
+                    onDismissRequest = {
+                        showSheet = false
+                    },
+                    sheetState = sheetState,
+
+                ) {
+
+                    Button(onClick = {
+                        scop.launch {
+                            sheetState.hide()
+                        }.invokeOnCompletion {
+                            if (!sheetState.isVisible){
+                                showSheet = false
+                            }
+                        }
+
+                    }) {
+
+                    }
+
+                    Text(
+                        text = "Bottom Sheet is Hide")
+                }
+            }
+
+
+        }
+    }
+}
